b7d6fd20181e73eb855e97bad479e5d88e3cefd4
Edoardo006 <122224245+Edoardo006@users.noreply.github.com>
2024-10-02 16:55:52 +0200

    Es 5 iterazione

---
+++ es5_iterazione.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	int n1, n2;
+	do
+	{
+		printf("inserisci n1\n");
+		scanf("%d", &n1);
+		printf("inserisci n2\n");
+		scanf("%d", &n2);
+		printf("\n");
+	} while (n1 < 0 || n2 <= n1);
+
+	while (n1 <= n2)
+	{
+		printf("%d\n", n1);
+		n1++;
+	}
+
+	return 0;
+}
